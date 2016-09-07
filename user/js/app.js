24d91f97f13690544461aca25ab0e38e92bc8a2a
csdevs1 <csdevs1@gmail.com>
2016-09-07 16:56:44 -0400

    Modified Quote Form and added new stuff

--- user/js/app.js
+++ user/js/app.js
@@ -7,8 +7,10 @@ $(document).ready(function(){
 });
 
 var closeWindow=function(){
-    document.getElementById('textarea-box').style.display="none";
+    $('#quote-form').hide(500);
+    document.getElementById('add-quote').style.display='block';
 }
-var openWindow=function(){
-    document.getElementById('textarea-box').style.display="block";
+var openWindow=function(el){
+    $('#quote-form').show(500);
+    el.style.display='none';
 }
