09f5de19e91f709284e689149239896f729767a3
csdevs1 <csdevs1@gmail.com>
2016-09-22 10:00:44 -0400

    Added Authors panel

--- c9a28e7f0dbc3ed20a161351c4f29a7b/21232f297a57a5a743894a0e4a801fc3/js/load.js
+++ c9a28e7f0dbc3ed20a161351c4f29a7b/21232f297a57a5a743894a0e4a801fc3/js/load.js
@@ -1,20 +1,30 @@
 function english(msg="",el) {
     $('.lang-nav li').not(el).removeClass('active');
+    $('.has-submenu').not('#quote-menu').removeClass('active');
     $(el).addClass('active');
     document.getElementById("quotes-area").innerHTML="";
     $("#quotes-area").load("language/quotes-en.html",{msg:msg});
 }
 function spanish(msg="",el) {
     $('.lang-nav li').not(el).removeClass('active');
+    $('.has-submenu').not('#quote-menu').removeClass('active');
     $(el).addClass('active');
     document.getElementById("quotes-area").innerHTML="";
     $("#quotes-area").load("language/quotes-spa.html",{msg:msg});
 }
 function portuguese(msg="",el) {
     $('.lang-nav li').not(el).removeClass('active');
+    $('.has-submenu').not('#quote-menu').removeClass('active');
     $(el).addClass('active');
     document.getElementById("quotes-area").innerHTML="";
     $("#quotes-area").load("language/quotes-pt.html",{msg:msg});
+}
+function authors(msg="",el) {
+    $('.lang-nav li').not(el).removeClass('active');
+    $('.has-submenu').not(el).removeClass('active');
+    $(el).addClass('active');
+    document.getElementById("quotes-area").innerHTML="";
+    $("#quotes-area").load("content/authors.html",{msg:msg});
 }
 $(document).ready(function(){
     english('','#eng');
--- visitorsCountry.php
+++ visitorsCountry.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 function get_client_ip_server() {
     $ipaddress = '';
     if ($_SERVER['HTTP_CLIENT_IP'])
@@ -19,8 +21,8 @@ function get_client_ip_server() {
  
     return $ipaddress;
 }
-
 /*
+
 function get_visitors_country($ip){
     $visitors_data = json_decode(file_get_contents("https://freegeoip.net/json/$ip"),true);
     return $visitors_data;
@@ -30,14 +32,13 @@ function get_visitors_language($c_code){
     return $visitors_data;
 }
 
-
-$ip = get_client_ip_server();
-$visitors_country = get_visitors_country('201.208.16.137');
+$visitors_country = get_visitors_country('190.72.53.235');
 $visitors_language= get_visitors_language($visitors_country['country_code']);
 echo 'Country: '.$visitors_country['country_name'].'<br>';
 echo  'Language: '.$visitors_language['languages'][0];*/
-
+    
 // This Function is to be uploaded to the server
+
 function get_visitors_country($ip){
     $ch = curl_init();
     $url = "https://freegeoip.net/json/$ip";
@@ -62,7 +63,18 @@ $ip = get_client_ip_server();
 $visitors_country=get_visitors_country($ip);
 $visitors_language= get_visitors_language($visitors_country['country_code']);
 
-echo 'Country: '.$visitors_country['country_name'].'<br>';
-echo  'Language: '.$visitors_language['languages'][0];
+//echo 'Country: '.$visitors_country['country_name'].'<br>';
+//echo  'Language: '.$visitors_language['languages'][0];
+/*
+if(!isset($_SESSION['lang']) && empty($_SESSION['lang']))
+    $_SESSION['lang']=$visitors_language;
+
+if($_SESSION['lang']=='es'){
+    header('Location: es');
+}
+elseif($_SESSION['lang']=='pt')
+    header('Location: pt');
+else
+    header('Location: home');*/
 
 ?>
