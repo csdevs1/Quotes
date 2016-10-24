31d2c752cd82a566c3ea53ed3d2dd4c01af3abd5
csdevs1 <csdevs1@gmail.com>
2016-10-24 17:42:09 -0400

    Topic page and Topic page by letter

---
+++ topicsPage.php
@@ -0,0 +1,87 @@
+<?php
+    require_once('AppClasses/AppController.php');
+    $obj = new AppController();
+    class HeadTags{
+        public function titlePage($el) {
+            return $el." at PortalQuote";
+        }
+        public function meta_description($el) {
+            return $el;
+        }
+    }
+
+    function add3dots($string, $repl, $limit){
+        if(strlen($string) > $limit){
+            return substr($string, 0, $limit) . $repl;
+        }
+        else {
+            return $string;
+        }
+    }
+
+    $alphas = range('A', 'Z');
+    $meta_tags = new HeadTags();
+    $title = $meta_tags->titlePage('Quotes by Topics');
+    $description = $meta_tags->meta_description("Check out our great collection of topics regarding Love, Inspiration, Motivation and more. Share with your friends on Facebook, Twitter, Instagram...");
+    $image = "https://portalquote.com/images/thumbnail.png";
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <?php include 'layouts/head.php'; ?>
+    </head>
+    <body>
+        <section class="background" id="banner" role="banner">
+            <?php include 'layouts/nav.php'; ?>
+            <!-- -->
+        </section>
+        
+        <!-- SIGN UP FORM -->
+        <?php include 'layouts/signup.php'; ?>
+        <!-- -->
+        
+        <section role="main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 title title-topic">
+                        <h2>TOPICS</h2>
+                    </div>
+                    <div class="col-xs-12">
+                        <span>Check out our great collection of topics regarding love, inspiration, motivation, friendship, success and more. <strong>Enjoy</strong>!</span>
+                    </div>
+                    <?php
+                        foreach($alphas as $key=>$val){
+                            $topics = $obj->custom("SELECT quotesTopicEN.topicID,topics_en.topicName,topics_en.seo_url,topics_en.topicID,COUNT(quotesTopicEN.topicID) AS 'cnt' FROM quotesTopicEN INNER JOIN topics_en ON topics_en.topicID=quotesTopicEN.topicID WHERE topics_en.topicName LIKE '".$val."%' GROUP BY quotesTopicEN.topicID ORDER BY COUNT(quotesTopicEN.topicID) DESC LIMIT 4;");
+                            if(isset($topics) && !empty($topics)){
+                    ?>
+                    <div class="col-xs-6 col-sm-4 col-md-3 list-topics">
+                        <h2><?php echo $val?></h2>
+                        <ul class="list-unstyled">
+                            <?php
+                                foreach($topics as $key2=>$val2){
+                            ?>
+                                <li><a href="/topic/quotes/<?php echo $topics[$key2]['seo_url']; ?>/1"><?php echo $topics[$key2]['topicName']; ?></a></li>
+                            <?php
+                                }
+                            ?>
+                        </ul>
+                        <a href="/topics/<?php echo strtolower($val); ?>/1">View more</a>
+                    </div>
+                    <?php
+                            }
+                        }
+                    ?>
+                </div>
+                
+                <div class="row explore-more">
+                    <div class="col-xs-12">Want to explore more?</div>
+                    <div class="col-xs-12"><a class="btn btn-primary">CHECKOUT OUR LIST OF KEYWORDS!</a></div>
+                </div>
+            </div>
+        </section>
+        
+        <!-- FOOTER -->
+        <?php include 'layouts/footer.php'; ?>
+    </body>
+</html>
---
+++ topicsPageByLetter.php
@@ -0,0 +1,103 @@
+<?php
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    require_once('AppClasses/AppController.php');
+    require_once 'AppClasses/Paginator.php';
+    $obj = new AppController();
+    class HeadTags{
+        public function titlePage($el) {
+            return $el." at PortalQuote";
+        }
+        public function meta_description($el) {
+            return $el;
+        }
+    }
+
+    function add3dots($string, $repl, $limit){
+        if(strlen($string) > $limit){
+            return substr($string, 0, $limit) . $repl;
+        }
+        else {
+            return $string;
+        }
+    }
+    
+    $meta_tags = new HeadTags();
+    $title = $meta_tags->titlePage('Find Quotes From Topics By Letter \''.$_GET['l'].'\'');
+    $description = $meta_tags->meta_description("Find the best quotes by topics which start with '".$_GET['l']."'. Share with your friends on Facebook, Twitter, Instagram...");
+        // Pagination
+            $limit = (isset( $_GET['limit'])) ? $_GET['limit'] : 12;
+            $page = (isset( $_GET['page'])) ? $_GET['page'] : 1;
+            $links = (isset( $_GET['links'])) ? $_GET['links'] : 7;
+            $Paginator  = new Paginator("topics_en WHERE topicName LIKE '".$_GET['l']."%'");
+            $topicsArr = $Paginator->getData("topics_en WHERE topicName LIKE '".$_GET['l']."%'","topicID",$limit,$page);
+    $topics = $topicsArr->data;
+    $image = "https://portalquote.com/images/thumbnail.png";
+	$folder='../../';
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <?php include 'layouts/head.php'; ?>
+    </head>
+    <body>
+        <section class="background" id="banner" role="banner">
+            <?php include 'layouts/nav.php'; ?>
+            <!-- -->
+        </section>
+        
+        <!-- SIGN UP FORM -->
+        <?php include 'layouts/signup.php'; ?>
+        <!-- -->
+        
+        <section role="main">
+            <div class="container">
+                <div class="row title"><h2>Topics Starting With <?php echo ucfirst($_GET['l']); ?></h2></div>
+                <div class="row">
+                    <ol class="breadcrumb">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/topics">Topics</a></li>
+                        <li class="active"><?php echo ucfirst($_GET['l']); ?></li>
+                    </ol>
+                </div>
+                <div class="row">
+                    <?php
+                    if(isset($topics) && !empty($topics)){
+                        foreach($topics as $key=>$val){
+                            $images = $obj->find_by('topicsImages','tID',$topics[$key]['topicID']);
+                            $num = rand(0,count($images)-1);
+                    ?>
+                        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 card-container">
+                            <div class="col-xs-12 card-header" style="background-image:url('<?php echo $images[$num]['img_url']; ?>');">
+                                <h3><a href="/topic/quotes/<?php echo $topics[$key]['seo_url']; ?>/1"><?php echo $topics[$key]['topicName']; ?></a></h3>
+                            </div>
+                            <div class="col-xs-12 card-content">
+                                <div class="addthis_sharing_toolbox col-xs-8 col-md-8" data-url="https://portalquote.com/topic/quotes/<?php echo $topics[$key]['seo_url']; ?>/1" data-title="<?php echo "Find the best quotes about ".strtolower($topics[$key]['topicName']).", ".$topics[$key]['keywords']."and more by the best authors and famous people"; ?>. Share with your friends on Facebook, Twitter, Instagram..."></div>
+                                <a class="btn btn-primary" href="/topic/quotes/<?php echo $topics[$key]['seo_url']; ?>/1">View</a>
+                            </div>
+                        </div>
+                    <?php
+                        }
+                    } else{
+                        echo "<h1>Sorry, authors not found!</h1>";
+                    }
+                    ?>
+                </div>
+            </div>
+        </section>
+        <div class="container">
+            <nav aria-label="Page navigation">
+                <?php echo $Paginator->createLinks($links, 'pagination pagination-sm',dirname($_SERVER[REQUEST_URI])); ?> 
+            </nav>
+        </div>
+        
+        <!-- FOOTER -->
+        <?php include 'layouts/footer.php'; ?>
+    </body>
+</html>
+<?php
+} else{
+    include('404.html');
+    exit();
+}
+?>
